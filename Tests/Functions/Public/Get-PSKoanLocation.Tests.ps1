26fb11d2cb35753ba1eee16169ff9955f8a3c21c
vexx32 <32407840+vexx32@users.noreply.github.com>
2018-12-13 22:51:57 -0500

    TESTS: Add tests for Get-/Set-Location

---
+++ Tests/Functions/Public/Get-PSKoanLocation.Tests.ps1
@@ -0,0 +1,27 @@
+﻿using module PSKoans
+
+Describe 'Get-PSKoanLocation' {
+
+    Context '$script:LibraryFolder is defined' {
+        BeforeAll {
+            $TestLocation = 'TestDrive:/PSKoans'
+            Set-PSKoanLocation -Path $TestLocation
+        }
+
+        It 'should return the stored location' {
+            Get-PSKoanLocation | Should -Be $TestLocation -Because 'it was set to this location previously'
+        }
+    }
+
+    Context '$script:LibraryFolder is not defined' {
+        BeforeAll {
+            InModuleScope 'PSKoans' {
+                Remove-Variable -Scope Script -Name 'LibraryFolder'
+            }
+        }
+
+        It 'should throw an error when called' {
+            { Get-PSKoanLocation } | Should -Throw -ExpectedMessage 'PSKoans folder location has not been defined'
+        }
+    }
+}
---
+++ Tests/Functions/Public/Set-PSKoanLocation.Tests.ps1
@@ -0,0 +1,50 @@
+﻿using module PSKoans
+
+Describe 'Set-PSKoanLocation' {
+
+    Context 'Specified Folder Exists' {
+        BeforeAll {
+            $Location = 'TestDrive:/Test/PSKoans'
+            New-Item -Path $Location -ItemType Directory > $null
+
+            Set-PSKoanLocation -Path $Location
+        }
+
+        It 'should set the location correctly' {
+            Get-PSKoanLocation | Should -Be $Location
+        }
+
+        It 'should set the module-scoped LibraryFolder variable' {
+            InModuleScope 'PSKoans' {
+                $script:LibraryFolder | Should -Be $Location
+            }
+        }
+    }
+
+    Context 'Specified Folder Doesn''t Exist' {
+        BeforeAll {
+            $Location = 'TestDrive:/Test/PSKoans'
+
+            Set-PSKoanLocation -Path $Location
+        }
+
+        It 'should set the location correctly' {
+            Get-PSKoanLocation | Should -Be $Location
+        }
+
+        It 'should set the module-scoped LibraryFolder variable' {
+            InModuleScope 'PSKoans' {
+                $script:LibraryFolder | Should -Be $Location
+            }
+        }
+    }
+
+    Context 'Specified Location is an Invalid Path' {
+
+        It 'should throw an error' {
+            $Location = 'TestDrive:::::\\\XD^^*#&'
+
+            { Set-PSKoanLocation -Path $Location } | Should -Throw -ExpectedMessage 'not found'
+        }
+    }
+}
